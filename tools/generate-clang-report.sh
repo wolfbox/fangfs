51ce9aa70b31dff4dc30fded96d659307503ebf6
Andrew Aldridge <i80and@foxquill.com>
2014-10-10 21:54:15 -0400

    Add simple static analysis tooling

---
+++ tools/generate-clang-report.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+mkdir -p reports/clang
+mkdir -p build_clang_scan
+cd build_clang_scan
+rm -rf ./*
+
+cmake -DCMAKE_C_COMPILER=ccc-analyzer ..
+scan-build -maxloop 20 -enable-checker alpha -o ../reports/clang/ make
