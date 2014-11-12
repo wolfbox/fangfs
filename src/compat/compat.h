b3e59f78b749e94ba7e4f3f06bf4a85b55c21e57
Andrew Aldridge <i80and@foxquill.com>
2014-11-11 21:57:04 -0500

    Use cmake-defined platform macros instead of cpp macro detection.
    Allows cppcheck to run without whining.

--- src/compat/compat.h
+++ src/compat/compat.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <unistd.h>
 #include <dirent.h>
+#include <unistd.h>
 #include <sys/sysctl.h>
 
 /// Broken operating systems like OSX don't support this
@@ -10,18 +10,18 @@ DIR* fdopendir(int fd);
 #endif
 
 inline size_t get_memory_size() {
-#if defined(_SC_PHYS_PAGES) && defined(_SC_PAGESIZE)
+#ifdef HAVE_SC_PHYS_PAGES
     {
         long pages = sysconf(_SC_PHYS_PAGES);
         long page_size = sysconf(_SC_PAGE_SIZE);
         if(pages < 0 || page_size < 0) { return 0; }
         return pages * page_size;
     }
-#elif defined(CTL_HW)
+#elif defined (HAVE_HW_MEMSIZE)
     {
         int mib[2];
         mib[0] = CTL_HW;
-        mib[1] = HW_PHYSMEM;
+        mib[1] = HW_MEMSIZE;
         size_t result;
         size_t len = sizeof(result);
         if(sysctl(mib, 2, &result, &len, NULL, 0) < 0) {
